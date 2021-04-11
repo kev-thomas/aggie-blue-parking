fdf3fc3a5fd2b842d16b439214c92bdf6cade74d
kev-thomas <ktomas1713@gmail.com>
2021-04-10 18:53:58 -0600

    bug fixes with fonts, vuetify and session

--- front/src/main.js
+++ front/src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
+import vuetify from '@/plugins/vuetify';
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
 import router from './router'
 import session from './plugins/session'
 import token from './plugins/jwt'
