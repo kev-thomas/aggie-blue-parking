ccf2686e64e5429da2ee6a9710148d96e48d7d4f
Spencer Hanson <spencerbhans@gmail.com>
2021-04-21 00:09:09 -0600

    Filled out Payment.vue, still need to add proper communication with the addMoney endpoint and apply rules to the textfields.:

--- front/src/views/Payment.vue
+++ front/src/views/Payment.vue
@@ -1,12 +1,47 @@
 <template>
   <v-card>
-    HOWDY
-  </v-card>
-  
+    <v-form ref="Pform">
+      <v-card-title>Payment Information</v-card-title>
+      <v-card-text>
+        <v-text-field
+          v-model="payment['fullName']"
+          label="Full name"
+          required
+        ></v-text-field>
+
+        <v-text-field
+          v-model="payment['ccNumber']"
+          label="Credit card number"
+          required
+        ></v-text-field>
+        <v-text-field
+          v-model="payment['exprDate']"
+          label="Expiration date"
+          required
+        ></v-text-field>
+
+        <v-text-field
+          v-model="payment['secCode']"
+          label="Security code"
+          required
+        ></v-text-field>
+        <v-text-field
+          v-model="payment['amount']"
+          label="$ Amount"
+          required
+        ></v-text-field>
+      </v-card-text>
+      <v-btn
+        :disabled="!valid && loading"
+        @click="pay"
+        :loading="loading"
+      >Add money</v-btn>
+    </v-form>
+  </v-card> 
 </template>
 
 <script>
-import parking from '../plugins/axios'
+// import parking from '../plugins/axios'
 export default {
   name: "Payment",
 
@@ -21,6 +56,13 @@ export default {
       renter: 'true',
       owner: 'false',
     },
+    payment: {
+      fullName: '',
+      ccNumber: '',
+      exprDate: '',
+      secCode: '',
+      amount: '',
+    },
     person: null,
     valid: true,
     loading: false,
@@ -28,16 +70,17 @@ export default {
   }),
 
   methods: {
-    async register() {
-      try {
-            let response = await parking.post('register', this.user);
-            // this.person = response.data;
-            console.log(response.data);
-          }
-          catch(error) {
-            console.log('OHNO');
-            console.log(error);
-          }
+    async pay() {
+      console.log("WIP");
+    //   try {
+    //         let response = await parking.post('register', this.user);
+    //         // this.person = response.data;
+    //         console.log(response.data);
+    //       }
+    //       catch(error) {
+    //         console.log('OHNO');
+    //         console.log(error);
+    //       }
     }
   }
 }
